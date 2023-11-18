c5f99d16c1306e042a5861d021aef086f2bca5b2
shielamaebacuds <shielamaebacuds@gmail.com>
2023-11-19 00:04:53 +0800

    changed labels

---
+++ src/Expiration.js
@@ -0,0 +1,10 @@
+import Header from "./Header";
+import './css/expiration.css'
+export default function Expiration() {
+  return (
+    <div className="expiration">
+        <Header headerName={"Expiration"}/>
+      <div className="body">Expiration</div>
+    </div>
+  );
+}
