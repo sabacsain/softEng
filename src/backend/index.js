d8e9764a15576e676d361929ad87aea62a1f1a4f
roxanne yambao <120180179+nozomi-jpg@users.noreply.github.com>
2023-12-16 21:59:43 +0800

    Update index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -158,7 +158,14 @@ app.post("/addType", (req,res)=>{
   })
 
 });
-
+app.get("/expiration-table", (req,res)=>{
+    const q = "SELECT `Exp_ID`, `Name_inventory`, `Pcs_inventory`, `Kg_inventory`, expired.Price, `Expiration_date` FROM expired LEFT JOIN inventory ON expired.Inventory_ID = inventory.Inventory_ID;"
+    db.query(q,(err,data)=>{
+      if(err) return res.json(err)
+      return res.json(data)
+    })
+  
+  });
 
 
 app.listen(8081, () => {
