e82304cc32f89500218e560e9de4fafb896645d6
roxanne yambao <120180179+nozomi-jpg@users.noreply.github.com>
2023-12-16 21:58:03 +0800

    Update Expiration.js

--- src/Expiration.js
+++ src/Expiration.js
@@ -1,43 +1,42 @@
+import { useEffect, useState } from "react";
 import "./css/expiration.css";
-import { useState } from "react";
+import axios from 'axios';
 import { SearchBar, SortBy } from "./Search";
 import Table from "./Table";
 import { SecondHeader  } from "./Header";
 
 //sample columns
 const columns = [
-  "ID",
+  "Exp_ID",
   "Ingredient",
-  "Type",
   "Pcs",
   "Kgs",
   "Price",
-
-  "Expiration Date",
+  "Expiration Date"
 ];
 
-//sample data
-const inventory_items = [
-  {
-    id: 23,
-    Ingredient: "Carrot",
-    Type: "Vegetable",
-    Pcs: 100,
+// //sample data
+// const inventory_items = [
+//   {
+//     id: 23,
+//     Ingredient: "Carrot",
+//     Type: "Vegetable",
+//     Pcs: 100,
 
-    Kgs: 0,
-    Price: 250,
-    Expiration: "2023-12-02",
-  },
-  {
-    id: 12,
-    Ingredient: "Ground Beef",
-    Type: "Meat",
-    Pcs: 0,
-    Kgs: 25,
-    Price: 500,
-    Expiration: "2023-01-02",
-  },
-];
+//     Kgs: 0,
+//     Price: 250,
+//     Expiration: "2023-12-02",
+//   },
+//   {
+//     id: 12,
+//     Ingredient: "Ground Beef",
+//     Type: "Meat",
+//     Pcs: 0,
+//     Kgs: 25,
+//     Price: 500,
+//     Expiration: "2023-01-02",
+//   },
+// ];
 
 export default function Expiration() {
   return (
@@ -53,6 +52,34 @@ export default function Expiration() {
 function TableSection() {
   //table section with form section
   //for query
+    //for waste
+    const[expired_items, setExpiredItems] = useState(
+      [
+        {
+        id: 0,
+        ingredient: "",     
+        pieces: 0,
+        weight: 0,
+        price: 0,
+        expiration:""
+        
+        }
+      ]
+    );
+  //display ingredients from inventory
+  useEffect(()=>{
+    const fetchAllExpired = async () => {
+      try{
+        const res = await axios.get("http://localhost:8081/expiration-table")
+        setExpiredItems(res.data)
+      } catch(err){
+        console.log(err)
+      }
+    };
+
+    fetchAllExpired();
+  }, []);
+
   const [searchValue, setSearchValue] = useState(""); //holds the value you type in the searchbox
   const [searchedColumn, setSearchedColumn] = useState("Ingredient"); //hold the chosen column beside the sortby
   const [order, setOrder] = useState("ASC"); //holds the chosen order, ASC or DESC
@@ -93,7 +120,7 @@ function TableSection() {
         {/* null - even if record is clicked, no data will be received from the table unlike in inventory and today's waste */}
         <Table
           columns={columns}
-          data={inventory_items}
+          data={expired_items}
           handleClickedRecord={null}
         />
       </div>
