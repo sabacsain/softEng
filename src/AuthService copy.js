64fb43fa928824333c768cd13f7441758ddc51d7
Shan Allen Bacsain <48052318+sabacsain@users.noreply.github.com>
2023-12-16 10:13:31 +0800

    add - darkmode

---
+++ src/AuthService copy.js
@@ -0,0 +1,41 @@
+// // AuthService.js
+// const db = require("./db").default
+// const API_URL = "http://localhost:3001"; // Replace with your server's API endpoint
+
+// class AuthService {
+//   async login(username, password) {
+//     const response = await fetch(`${API_URL}/login`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ username, password }),
+//     });
+
+//     if (!response.ok) {
+//       const error = await response.json();
+//       throw new Error(error.message || "Login failed");
+//     }
+
+//     return response.json();
+//   }
+
+//   async signup(username, password) {
+//     const response = await fetch(`${API_URL}/signup`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ username, password }),
+//     });
+
+//     if (!response.ok) {
+//       const error = await response.json();
+//       throw new Error(error.message || "Signup failed");
+//     }
+
+//     return response.json();
+//   }
+// }
+
+// export default new AuthService();
