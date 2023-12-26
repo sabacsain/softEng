22ff830e1671c50685850e6c5689a2ac510b32ad
roxanne yambao <120180179+nozomi-jpg@users.noreply.github.com>
2023-12-26 13:28:05 +0800

    Update Inventory.js

--- src/Inventory.js
+++ src/Inventory.js
@@ -7,19 +7,19 @@ import Table from "./Table.js";
 import { format } from "date-fns";
 import CrudButtons from "./CrudButtons.js";
 import axios from 'axios';
+import moment from 'moment';
 
 //sample columns
 const columns = [
   "ID",
   "Ingredient",
   "Type",
+  "Type ID",
   "Pcs",
   "Kgs",
   "Price",
-
   "Expiration Date",
 ];
-
 //sample types
 const types = ["Vegetable", "Meat", "A", "B", "C"];
 
@@ -47,7 +47,7 @@ function InventoryHeader() {
   return (
     <>
       <div class="inventory-header">
-        <h1>Inventory palitan niyo nalang pag magulo na HAHHA</h1>
+        <h1>Inventory</h1>
         <div class="inventory-notif-wrapper">
           <div class="notif">
             {/* {render span only when there are new notif that user has not seen yet} */}
@@ -67,6 +67,38 @@ function InventoryHeader() {
 }
 
 function NotificationComponent({ handleOpenNotif }) {
+  const [expiringThisDay, setExpiringThisDay] = useState([]);
+  const [expiringThisWeek, setExpiringThisWeek] = useState([]);
+  
+  //get the ingredients expiring today
+  useEffect(()=>{
+    const fetchExpiringToday = async () => {
+      try{
+        const res = await axios.get("http://localhost:8081/expiringToday")
+        setExpiringThisDay(res.data)
+      } catch(err){
+        console.log(err)
+      }
+    };
+
+    fetchExpiringToday();
+  }, []);
+
+
+  //get the ingredients expiring week
+  useEffect(()=>{
+    const fetchExpiringWeek = async () => {
+      try{
+        const res = await axios.get("http://localhost:8081/expiringWeek")
+        setExpiringThisWeek(res.data)
+      } catch(err){
+        console.log(err)
+      }
+    };
+
+    fetchExpiringWeek();
+  }, []);
+
   return createPortal(
     <>
       {/* <!-- Overlay background --> */}
@@ -79,38 +111,20 @@ function NotificationComponent({ handleOpenNotif }) {
           <Link to="/inventory/expiration-table">See all expired items</Link>
         </div>
 
-        <h3 class="h3-inventory tomorrow">Tomorrow (10)</h3>
+        <h3 class="h3-inventory tomorrow">Tomorrow ({expiringThisDay.length})</h3>
         <hr></hr>
         <div className="notif-entries-wrapper">
-          <div>Strawberryf jkdljfksf</div>
-          <div>Strawberry fsdfsdf</div>
-          <div>Strawberr f dfsfy</div>
-          <div>Strawberry fdfsf</div>
-          <div>Strawberry</div>
-          <div>Strawberry</div>
-          <div>Strawberry</div>
-          <div>Strawberry</div>
-          <div>Strawberrdfsdfdsfy</div>
-          <div>Strawbersfsdry</div>
-
-          {/* <!-- DATABASE LIST --> */}
+          {expiringThisDay.map((expiringThisDay) => (
+            <div>{expiringThisDay.Name_inventory}</div>
+          ))}
         </div>
 
         <h3 class="h3-inventory thisweek">This Week</h3>
         <hr></hr>
         <div className="notif-entries-wrapper">
-          <div>Strawberryf jkdljfksf</div>
-          <div>Strawberry fsdfsdf</div>
-          <div>Strawberr f dfsfy</div>
-          <div>Strawberry fdfsf</div>
-          <div>Strawberry</div>
-          <div>Strawberry</div>
-          <div>Strawberry</div>
-          <div>Strawberry</div>
-          <div>Strawberrdfsdfdsfy</div>
-          <div>Strawbersfsdry</div>
-
-          {/* <!-- DATABASE LIST --> */}
+        {expiringThisWeek.map((expiringThisWeek) => (
+            <div>{expiringThisWeek.Name_inventory}</div>
+          ))}
         </div>
 
         <button class="notif-close-btn" onClick={handleOpenNotif}>
@@ -130,6 +144,7 @@ function TableSection() {
     [
       {
       id: 0,
+      typeId: 1,
       ingredient: "",
       type: "Vegetable",
       weight: 0,
@@ -140,28 +155,25 @@ function TableSection() {
     ]
   );
 
-  //for query
-  const [searchValue, setSearchValue] = useState(""); //holds the value you type in the searchbox
-  const [searchedColumn, setSearchedColumn] = useState("Ingredient"); //hold the chosen column beside the sortby
-  const [order, setOrder] = useState("ASC"); //holds the chosen order, ASC or DESC
-  const [isPerishable, setIsPerishable] = useState(true); //hpersihable or non
-
-  const [clickedRecord, setInventoryRecord] = useState({
-    //holds attributes and values of the record u clicked from the table
-    id: 0,
-    ingredient: "",
-    type: "Vegetable",
-    weight: 0,
-    pieces: 0,
-    price: 0,
-    expiration: ""
-  });
-
-  //display ingredients from inventory
+  const [filteredItems, setFilteredItems] = useState([]);
+  //Display ingredients from inventory
   useEffect(()=>{
     const fetchAllIngredients = async () => {
       try{
         const res = await axios.get("http://localhost:8081/ingredients")
+        
+        //Rename the keys of the data object
+        res.data.forEach(Rename);
+        function Rename(item){
+          delete Object.assign(item, { id: item.Inventory_ID })['Inventory_ID'];
+          delete Object.assign(item, { ingredient: item.Name_inventory })['Name_inventory'];
+          delete Object.assign(item, { type: item.Type_name })['Type_name'];
+          delete Object.assign(item, { typeId: item.Type_ID })['Type_ID'];
+          delete Object.assign(item, { weight: item.Kg_inventory })['Kg_inventory'];
+          delete Object.assign(item, { pieces: item.Pcs_inventory })['Pcs_inventory'];
+          delete Object.assign(item, { price: item.Price })['Price'];
+          delete Object.assign(item, { expiration: moment.utc(item.Expiration_date).format('YYYY/MM/DD') })['Expiration_date'];
+        }
         setInventoryItems(res.data)
       } catch(err){
         console.log(err)
@@ -171,12 +183,29 @@ function TableSection() {
     fetchAllIngredients();
   }, []);
 
+  //for query
+  const [searchValue, setSearchValue] = useState(""); //holds the value you type in the searchbox
+  const [searchedColumn, setSearchedColumn] = useState("Ingredient"); //hold the chosen column beside the sortby
+  const [order, setOrder] = useState("ASC"); //holds the chosen order, ASC or DESC
+  const [isPerishable, setIsPerishable] = useState(true); //hpersihable or non
+
+  const [clickedRecord, setInventoryRecord] = useState({
+    //holds attributes and values of the record u clicked from the table
+    id: 0,
+    typeID: 1,
+    ingredient: "",
+    type: "Vegetable",
+    weight: 0,
+    pieces: 0,
+    price: 0,
+    expiration: ""
+  });
+
   const handleSearch = (e) => {
     //updates the searchValue variable when you type in the searcbox
     e.preventDefault(); //prevents page from loading after submit or enter
     setSearchValue((currentWord) => (currentWord = e.target.value));
   };
-
   const handleSortColumn = (e) => {
     //updates the searchedColumn variable when you choose from sortby dropdown
     setSearchedColumn((selectedOption) => (selectedOption = e.target.value));
@@ -202,21 +231,22 @@ function TableSection() {
     if (isthereAnActiveRow) {
       setInventoryRecord({
         id: item.id,
-        ingredient: item.Ingredient,
-        type: item.Type,
-        weight: item.Kgs,
-        pieces: item.Pcs,
-        price: item.Price,
-        expiration: item.Expiration,
+        ingredient: item.ingredient,
+        type: item.type,
+        typeId: item.typeId,
+        weight: item.weight,
+        pieces: item.pieces,
+        price: item.price,
+        expiration: item.expiration,
       });
     }
-
     //if no record is active(di naka-click), these are the default values for clickedRecord variable
     else {
       setInventoryRecord({
         id: 0,
         ingredient: "",
         type: "Vegetable",
+        typeId: 1,
         weight: 0,
         pieces: 0,
         price: 0,
@@ -226,11 +256,11 @@ function TableSection() {
   };
 
   //check mo kung nakuha mo nasa searchbox, at dropdowns !!!!!!
-  console.log("WORD YOU SEARCHED FOR: ", searchValue);
-  console.log("COLUMN YOU CHOSE TO SEARCH IN:", searchedColumn);
-  console.log("ORDER OF RECORDS U CHOSE (ASC OR DESC): ", order);
-  console.log("PERISHABLE OR NAH? ", isPerishable);
-  console.log("RECORD U CLICKED: ", clickedRecord);
+  // console.log("WORD YOU SEARCHED FOR: ", searchValue);
+  // console.log("COLUMN YOU CHOSE TO SEARCH IN:", searchedColumn);
+  // console.log("ORDER OF RECORDS U CHOSE (ASC OR DESC): ", order);
+  // console.log("PERISHABLE OR NAH? ", isPerishable);
+  // console.log("RECORD U CLICKED: ", clickedRecord);
 
   //use this to add, update, delete
   //currentFormRecord = dito ko nilagay yung values mula sa textboxes. bale dito, dineclare lang, not been used yet.
@@ -238,6 +268,7 @@ function TableSection() {
     id: 0,
     ingredient: "",
     type: "Vegetable",
+    typeId: 1,
     weight: 0,
     pieces: 0,
     price: 0,
@@ -251,6 +282,7 @@ function TableSection() {
       id: ID,
       ingredient: textfieldsValues.ingredient_field,
       type: textfieldsValues.type_field,
+      typeId: textfieldsValues.type_id,
       weight:
         textfieldsValues.quantity_dropdown === "Kg" //if kg yung nasa dropdown, 0 na value ng PCS
           ? textfieldsValues.quantity_field
@@ -291,7 +323,7 @@ function TableSection() {
       if(res.data === "Failed") {
         alert("This type of ingredient already exists.")
       } else{
-        console.log(res.data)
+        alert("Successfully added new ingedient.")
       }
     })
     .catch((error) => {
@@ -302,25 +334,35 @@ function TableSection() {
 
   //function for updating the currentFormRecord to the database
   const updateRecord = (record) => {
-    //insert code to update record from database
-    console.log("UPDATE THIS ID ->", record.id);
-    console.log("UPDATED DETAILS:", record);
+    axios.post('http://localhost:8081/updateInventory', record)
+      .then((res) => {
+        alert("Successfully Updated Record.")
+      })
+      .catch((error) => {
+        alert('Error during updating record:', error);
+        // Add additional error handling as needed
+      });
   };
 
   //function for deleting the currentFormRecord to the database
   const deleteRecord = (record) => {
-    //insert code to update record from database
-    console.log("DELETE THIS ID:", record.id);
-    console.log("DELETE THIS RECORD:", record);
+    axios.post('http://localhost:8081/deleteInventory', record)
+      .then((res) => {
+        alert("Successfully Deleted Record.")
+      })
+      .catch((error) => {
+        alert('Error during deleting record:', error);
+        // Add additional error handling as needed
+      });
   };
 
   return (
     <>
-<div id="inventory-search-and-sorting">
-        <SearchBar handleSearch={handleSearch} 
-        listOfData = {inventory_items}/>
+      <div id="inventory-search-and-sorting">
+        <SearchBar handleSearch={handleSearch} inventory_items={inventory_items} setFilteredItems={setFilteredItems}/>
         <SortBy
           options={columns}
+          data={filteredItems}
           handleSortColumn={handleSortColumn}
           handleOrder={handleOrder}
           currentOrder={order}
@@ -331,8 +373,8 @@ function TableSection() {
       {/* change column and data props */}
       <div className="inventory-table-wrapper">
         <Table
-          columns={columns}
-          data={inventory_items}
+          columns={Object.keys(inventory_items[0])}
+          data={filteredItems || []}
           handleClickedRecord={handleClickedRecord}
         />
       </div>
@@ -347,15 +389,48 @@ function TableSection() {
 function FormSection({ clickedRecord, handleSetInventoryRecord }) {
   //if not 0, use this for updating and deleting record; else, add new record
   const currentID = clickedRecord.id;
+  
+  //for types
+  const[types, setTypes] = useState(
+    [
+      {
+      id: 0,
+      type_name: "Vegetable"
+      }
+    ]
+  );
 
   //variable for tracking the textfields, if may changes sa fields dito i-uupdate
   const [textfields, setTextFieldsValues] = useState({
     ingredient_field: "",
     type_field: "Vegetable",
+    type_id: 1,
     price_field: 0,
     quantity_field: "",
     quantity_dropdown: "",
     expiration_picker: format(new Date(), "yyyy-MM-dd"),
+  });
+
+  //display list of type of wastes
+  useEffect(()=>{
+    const fetchAllTypes = async () => {
+      try{
+        const res = await axios.get("http://localhost:8081/types")
+        //Rename the keys of the data object
+        res.data.forEach(Rename);
+        function Rename(item){
+          delete Object.assign(item, { id: item.Type_ID })['Type_ID'];
+          delete Object.assign(item, { type_name: item.Type_name })['Type_name'];
+          delete Object.assign(item, { perishable: (item.Is_perishable == 0) ? "false": "true" })['Is_perishable'];
+        }
+
+        setTypes(res.data)
+      } catch(err){
+        console.log(err)
+      }
+    };
+
+    fetchAllTypes();
   });
 
   //if clickedRecord changes (nagclick ka ng iba or inunclick mo yung record), this will execute
@@ -365,6 +440,7 @@ function FormSection({ clickedRecord, handleSetInventoryRecord }) {
     setTextFieldsValues(() => ({
       ingredient_field: clickedRecord.ingredient,
       type_field: clickedRecord.type,
+      type_id: clickedRecord.typeId,
       price_field: clickedRecord.price,
       quantity_field:
         clickedRecord.weight > 0 ? clickedRecord.weight : clickedRecord.pieces,
@@ -419,13 +495,13 @@ function FormSection({ clickedRecord, handleSetInventoryRecord }) {
                 {/* loops through each type of waste and being put as option */}
                 <select
                   id="details-type"
-                  value={textfields.type_field}
+                  value={textfields.type_id}
                   onChange={(e) =>
-                    handleFieldChanges("type_field", e.target.value)
+                    handleFieldChanges("type_id", e.target.value)
                   }
                 >
                   {types.map((type) => (
-                    <option value={`${type}`}>{type}</option>
+                    <option value={`${type.id}`}>{type.type_name}</option>
                   ))}
                 </select>
               </div>
