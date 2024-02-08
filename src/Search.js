b3243b8dfc1231509e2703268f345b2a0902c623
roxanne yambao <120180179+nozomi-jpg@users.noreply.github.com>
2024-02-08 20:24:16 +0800

    removed perishable

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const SearchBar = ({ inventory_items=[], setFilteredItems }) => {
-    const [searchValue, setSearchValue] = useState("");
-  
-    useEffect(() => {
-      // Filter the inventory items based on the search value
-      const filteredItems = inventory_items.filter(item =>
-        Object.values(item).some(value =>
-          value.toString().toLowerCase().includes(searchValue.toLowerCase())
-        )
+const SearchBar = ({
+  inventory_items = [],
+  setFilteredItems,
+  searchedColumn,
+  order,
+}) => {
+  const [searchValue, setSearchValue] = useState("");
+  useEffect(() => {
+    // Filter the inventory items based on the search value and searchedColumn
+    const filteredItems = inventory_items.filter((item) => {
+      const columnValue = item[searchedColumn.toLowerCase()];
+
+      return (
+        columnValue !== undefined &&
+        columnValue.toString().toLowerCase().includes(searchValue.toLowerCase())
       );
-      setFilteredItems(filteredItems);
-    }, [searchValue, inventory_items, setFilteredItems]);
+    });
+    // Sort filteredItems based on the searchedColumn
+    const sorted_filteredItems = filteredItems.sort((a, b) => {
+      const columnA = a[searchedColumn.toLowerCase()];
+      const columnB = b[searchedColumn.toLowerCase()];
+
+      // Check if the values are numbers and perform numeric sorting
+      if (!isNaN(columnA) && !isNaN(columnB)) {
+        return order === "ASC" ? columnA - columnB : columnB - columnA;
+      }
+
+      // Perform string sorting
+      const valueA = columnA.toString().toLowerCase();
+      const valueB = columnB.toString().toLowerCase();
+
+      return order === "ASC"
+        ? valueA.localeCompare(valueB)
+        : valueB.localeCompare(valueA);
+    });
+
+    setFilteredItems(sorted_filteredItems);
+  }, [searchValue, inventory_items, setFilteredItems, searchedColumn, order]);
+
   return (
     <div className="searchbox-wrapper">
       <span className="search-icon">&#x2315;</span>
@@ -20,11 +47,11 @@ const SearchBar = ({ inventory_items=[], setFilteredItems }) => {
         className="searchbox"
         placeholder="Search item here"
         value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)} 
+        onChange={(e) => setSearchValue(e.target.value)}
       ></input>
     </div>
   );
-}
+};
 function SortBy({ options, handleSortColumn, handleOrder, currentOrder }) {
   return (
     <div className="sort-by-wrapper">
@@ -48,19 +75,19 @@ function SortBy({ options, handleSortColumn, handleOrder, currentOrder }) {
   );
 }
 
-function Filter({ handleIsPerishable }) {
-  return (
-    <div className="sort-by-wrapper">
-      <label for="filter-perishable">Filter:</label>
-      <select id="filter-perishable" onChange={handleIsPerishable}>
-        <option className="sort-option" value="Perishable">
-          Perishable
-        </option>
-        <option className="sort-option" value="Non-perishable">
-          Non-Perishable
-        </option>
-      </select>
-    </div>
-  );
-}
-export {SearchBar,SortBy, Filter};
+// function Filter({ handleIsPerishable }) {
+//   return (
+//     <div className="sort-by-wrapper">
+//       <label for="filter-perishable">Filter:</label>
+//       <select id="filter-perishable" onChange={handleIsPerishable}>
+//         <option className="sort-option" value="Perishable">
+//           Perishable
+//         </option>
+//         <option className="sort-option" value="Non-perishable">
+//           Non-Perishable
+//         </option>
+//       </select>
+//     </div>
+//   );
+// }
+export { SearchBar, SortBy };
